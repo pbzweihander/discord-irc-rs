dbc015eba154d27888c80439769259145f84e575
rusty <rusty@spoqa.com>
2019-12-22 01:46:31 +0900

    Show user's server nickname, not discord name

--- src/discord.rs
+++ src/discord.rs
@@ -29,13 +29,25 @@ impl DiscordHandler {
 impl EventHandler for DiscordHandler {
     fn message(&self, ctx: Context, msg: Message) {
         if !msg.author.bot && msg.channel_id == self.config.channel_id {
-            let mut content = msg.content;
+            let Message {
+                mut content,
+                guild_id,
+                author,
+                attachments,
+                mentions,
+                mention_roles,
+                ..
+            } = msg;
+            let Context { http, cache, .. } = ctx;
+            let name = guild_id
+                .and_then(|guild_id| author.nick_in(http, guild_id))
+                .unwrap_or_else(|| author.name);
 
-            for user in msg.mentions {
+            for user in mentions {
                 content = content.replace(&format!("{}", user.id), &user.name);
             }
-            for role_id in msg.mention_roles {
-                if let Some(role) = role_id.to_role_cached(&ctx.cache) {
+            for role_id in mention_roles {
+                if let Some(role) = role_id.to_role_cached(&cache) {
                     content = content.replace(&format!("{}", role.id), &role.name);
                 }
             }
@@ -43,21 +55,16 @@ impl EventHandler for DiscordHandler {
             let lines = content
                 .split('\n')
                 .map(|s| Cow::Borrowed(s))
-                .chain(msg.attachments.into_iter().map(|at| at.url).map(Into::into));
+                .chain(attachments.into_iter().map(|at| at.url).map(Into::into));
 
             for line in lines {
-                let name = &msg.author.name;
-
-                if self.config.ignores.contains(name) {
-                    debug!("DIS| <{}(ignored)> {}", msg.author.name, line);
+                if self.config.ignores.contains(&name) {
+                    debug!("DIS| <{}(ignored)> {}", name, line);
                 } else {
-                    info!("DIS> <{}> {}", msg.author.name, line);
+                    info!("DIS> <{}> {}", name, line);
 
                     let mut writer = self.irc_writer.clone();
-                    let msg = format!(
-                        "PRIVMSG {} :<{}> {}\n",
-                        self.irc_channel, msg.author.name, line,
-                    );
+                    let msg = format!("PRIVMSG {} :<{}> {}\n", self.irc_channel, name, line,);
                     spawn(async move {
                         writer
                             .send(msg)
